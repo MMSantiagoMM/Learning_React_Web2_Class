37a2f6e97a8a13efbe9172bf89076a9c9a2ab671
MMSantiagoMM <santiagomosquera2@gmail.com>
2023-11-18 13:14:13 -0500

    adding css class to component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Navigation(){
 
     return (
-      <div> My first component from react </div>
+      <div className="color"> My first component from react </div>
     )
   
 }
